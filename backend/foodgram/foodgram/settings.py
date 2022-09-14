da71d68a23e840508d1f21afb873b5581482d52a
Кирилл Резник <sunlight_1091@mail.ru>
2022-09-14 13:46:34 +0200

    Foodgram WF 15

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='django_secret')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- backend/foodgram/recipes/management/commands/imp_ingr.py
+++ backend/foodgram/recipes/management/commands/imp_ingr.py
@@ -1,53 +1,38 @@
 import csv
-import os
 
-from django.apps import apps
+from django.conf import settings
 from django.core.management import BaseCommand
 
-
-BASE_DICT = {'ingredients': ['Ingredient', 'recipes', 'filepath']}
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
 
-    def get_files(self, path):
-        files = []
-        for roots, dirs, file_names in os.walk(path):
-            for file_name in file_names:
-                if file_name.endswith('.csv'):
-                    file_path = os.path.join(roots, file_name)
-                    files.append(file_path)
-        return files
-
-    def transfer(self, base_item):
-        model_name = BASE_DICT[base_item][0]
-        app_name = BASE_DICT[base_item][1]
-        file_path = BASE_DICT[base_item][2]
-
-        model = apps.get_model(app_name, model_name)
-
-        with open(file_path, 'r', encoding='UTF-8') as csv_file:
-            reader = csv.reader(csv_file, delimiter=',', quotechar='"')
-
-            header = ('name', 'measurement_unit')
-
-            for element in reader:
-                new_object_dict = {key: value for key, value in
-                                   zip(header, element)}
-
-                try:
-                    model.objects.create(**new_object_dict)
-
-                except ValueError:
-                    self.stdout.write(
-                        self.style.ERROR('Error while transferring data')
-                    )
-
     def handle(self, *args, **options):
-        files = self.get_files(os.getcwd())
-        for file_path in files:
-            file_name = os.path.basename(file_path).split('.', 1)[0]
-            if file_name in BASE_DICT:
-                BASE_DICT[file_name][2] = file_path
+        with open(
+            f"{settings.BASE_DIR}/data/ingredients.csv",
+            "r",
+            encoding="utf-8",
+        ) as file:
+            file_reader = csv.reader(file)
+            for row in file_reader:
+                name, measurement_unit = row
+                Ingredient.objects.get_or_create(
+                    name=name,
+                    measurement_unit=measurement_unit)
+            file.close()
 
-        self.transfer('ingredients')
+        tags = (
+            ('Завтрак', '#AFB83B', 'breakfast'),
+            ('Обед', '#FAD000', 'dinner'),
+            ('Ужин', '#FF9933', 'supper'),
+        )
+
+        for tag in tags:
+            name, color, slug = tag
+            Tag.objects.get_or_create(
+                name=name,
+                color=color,
+                slug=slug
+            )
+        self.stdout.write(self.style.SUCCESS('Ингредиенты и тэги добавлены'))
